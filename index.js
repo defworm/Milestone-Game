e357ddd689e49f5d3bdfc98dde9cd37549263ea7
Crystal Korth <crystalkorth@gmail.com>
2022-06-25 14:03:08 -0500

    added green characters from class as stand ins, will modify after functionality of game confirmed.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+// Leaflet basic start up
+const map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
