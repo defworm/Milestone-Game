8eb0084578b63dc2b3db9d7b8e19fe57e6f9c333
Crystal Korth <crystalkorth@gmail.com>
2022-06-29 19:32:27 -0500

    added temp background image (will replace w/ map) and disabled scrolling

--- index.js
+++ index.js
@@ -10,11 +10,17 @@
 // }).addTo(map);
 
 // creating the main character and party members (from gifer.com)
-let mc = mainCharacter(100, 110);
-let c2= character2(100, 110);
-let c3 = character3(100, 110);
-let c4 = character4(100, 110);
-let c5 = charcter5 (100, 100);
+const mc = mainCharacter(100, 110);
+const c2= character2(100, 110);
+const c3 = character3(100, 110);
+const c4 = character4(100, 110);
+const c5 = charcter5 (100, 100);
 
+// const moveMainCharacter = async () => { 
+//     while (true){    
+//     await mainCharacter.walkUp(1400);
+// }
+// } 
 
+// moveMainCharacter();
 
