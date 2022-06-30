a5d1c696d938be50cc49c54593abd2f6e676be03
Crystal Korth <crystalkorth@gmail.com>
2022-06-29 20:29:25 -0500

    deleted all js code, starting all over w/ newImage.js

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
 
 
 
-
-// creating the main character and party members (from gifer.com)
-const mc = mainCharacter(100, 110);
-const c2= character2(100, 110);
-const c3 = character3(100, 110);
-const c4 = character4(100, 110);
-const c5 = charcter5 (100, 100);
-
-// const moveMainCharacter = async () => { 
-//     while (true){    
-//     await mainCharacter.walkUp(1400);
-// }
-// } 
-
-// moveMainCharacter();
-
--- newImage.js
+++ newImage.js
@@ -1,28 +1,5 @@
 function newImage(url){
     let image = document.createElement('img')
     image.src = url
-    document.body.append(image)
-    return image
+    image.style.position = 'absou'
 }
-
-function move(element){
-    element.style.position = 'relative'
-
-    function moveToCoordinates(left,bottom){
-        element.style.left = left + 'px'
-        element.style.bottom = bottom + 'px'
-    }
-    return {
-        to: moveToCoordinates
-    }
-}
-
-// gifs from gifer.com, animation changes made on ezgif.com
-// move(newImage('assets/mainCharacter.gif', 100, 250))
-
-// newImage('assets/mainCharacter.gif', 100, 250)
-// newImage('assets/left.gif', 100, 250)
-// newImage('assets/character2.gif', 100, 250)
-// newImage('assets/character3.gif', 100, 250)
-// newImage('assets/character4.gif', 100, 250)
-// newImage('assets/character5.gif', 100, 250)
