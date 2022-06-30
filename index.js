a7b7e0b4a546ce3fbcafdd0f82ea13d5e63aac94
Crystal Korth <crystalkorth@gmail.com>
2022-06-29 19:54:52 -0500

    removed all leaflet code

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 
 
-// Leaflet basic start up
-// const map = L.map('map').setView([51.505, -0.09], 5);
 
-
-// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     maxZoom: 19,
-//     attribution: '© OpenStreetMap'
-// }).addTo(map);
 
 // creating the main character and party members (from gifer.com)
 const mc = mainCharacter(100, 110);
